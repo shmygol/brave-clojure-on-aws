2aeb8b63f2f817cc31448c15627a7d3a72aa9caf
Ilya Shmygol <ishmygol@gmail.com>
2021-05-01 17:03:10 +0200

    Prepare a handler bootstrap

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
 # brave-clojure-on-aws
-Exercises from the book "Clojure for the brave and true" deployed on AWS serverless solutions
+Exercises from the book "Clojure for the brave and true" deployed on Sleipnir, i.e. AWS serverless solutions.
 
 ![Slepnir](https://user-images.githubusercontent.com/807663/115037584-e8232100-9ece-11eb-91aa-ae71b7bfb66a.png)
+
+## Requirements
+
+To be able to deploy the project, development dependencies have to be installed:
+
+1. Clojure
+2. Leiningen
 
 ## Deploying
 
 AWS Lambdas are managed with [Serverless](https://serverless.com/) framework.
 
-To build and deploy a stack
+To build and deploy a stack:
 ```
 lein uberjar
 npx sls deploy -v --stage dev
 ```
+
+Currently the uberjar contains handlers for all the lambda functions, which shall be improved later.
+
+## Development
+
+Every book chapter is a separated namespace and is deployed to a dedicated AWS lambda function e.g.,
+brave-clojure.chapter3 namespace for the 3rd chapter is deployed to an AWS lambda function named chapter3.
+
+## Run locally
+
+Is on the roadmap
+
+## Tests
+
+Is on the roadmap
---
+++ src/brave_clojure/chapter3.clj
@@ -0,0 +1,50 @@
+(ns brave-clojure.chapter3
+  "Exercises from Chapter 3"
+  (:gen-class
+    :methods [^:static [handler [java.util.Map] java.util.Map]]))
+
+(defn exercise-1
+  "1. Description for the exercise"
+  []
+  {:result "foo"})
+
+(defn exercise-2
+  "2. Description for the exercise"
+  []
+  {:result "foo"})
+
+(defn exercise-3
+  "3. Description for the exercise"
+  []
+  {:result "foo"})
+
+(defn exercise-4
+  "4. Description for the exercise"
+  []
+  {:result "foo"})
+
+(defn exercise-5
+  "5. Description for the exercise"
+  []
+  {:result "foo"})
+
+(defn exercise-6
+  "6. Description for the exercise"
+  []
+  {:result "foo"})
+
+(defn -handler
+  "AWS Lambda entry point.
+  event[\"exercise-number\"] - exercise numeber
+  event[\"arguments\"] - exercise arguments
+  "
+  [event]
+  (let [{:strs [exercise-number arguments]} event
+        exercises [nil
+                   exercise-1
+                   exercise-2
+                   exercise-3
+                   exercise-4
+                   exercise-5
+                   exercise-6]]
+    (apply (exercises exercise-number) arguments)))
